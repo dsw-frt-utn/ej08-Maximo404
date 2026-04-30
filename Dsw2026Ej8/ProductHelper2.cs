660623f4e9dd319217ac520132d165d353478174
Maximo Suleiman <MaximoNahimSuleiman@alu.frt.utn.edu.ar>
2026-04-30 20:16:39 -0300

    Problema 1

---
+++ Dsw2026Ej8/ProductHelper2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        //Metodo auxilr para el metodo ObtenerEtiquetaProducto, que formatea el precio a moneda local
+        private string FormatearPrecio(decimal price)
+        {
+            return price.ToString("C");
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProductHelper producto1 = new ProductHelper();
+
+            Console.WriteLine($"{producto1.ObtenerEtiquetaProducto(111, "Jugo Ades", 3000)}");
+
         }
     }
 }
