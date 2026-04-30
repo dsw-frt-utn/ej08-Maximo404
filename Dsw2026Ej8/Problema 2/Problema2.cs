54978226a8e960c1b67b0eec7ecb556f436ced26
Maximo Suleiman <MaximoNahimSuleiman@alu.frt.utn.edu.ar>
2026-04-30 20:29:14 -0300

    problema 2

---
+++ Dsw2026Ej8/Problema 2/Problema2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema_2
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            if(quantity < 0)
+            {
+                quantity = 0;
+            }
+
+            var producto = new { Code = productCode, Description = productDescription, Quantity = quantity, Total = quantity * unitPrice };
+            return $"{producto.Code} - {producto.Description} - {producto.Total:C}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,12 +1,18 @@
-﻿namespace Dsw2026Ej8
+﻿using Dsw2026Ej8.Problema_2;
+
+namespace Dsw2026Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Crear una instancia de ProductHelper
             ProductHelper producto1 = new ProductHelper();
-
             Console.WriteLine($"{producto1.ObtenerEtiquetaProducto(111, "Jugo Ades", 3000)}");
+
+            //Problema 2
+            Problema2 producto2 = new Problema2();
+            Console.WriteLine($"{producto2.CrearResumenVenta(112, "Bon o Bon", 10, 600)}");
 
         }
     }
